8c628f0141c275cbc826f5e9dcefade3a52218db
Shirin Zarghami <shirin@shirin.(none)>
2014-11-05 14:41:30 +0100

    cleanin version of eposid 6 off Laravel, it creates link from index view to show view

--- app/routes.php
+++ app/routes.php
@@ -10,36 +10,9 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-//Route::get('/', 'PagesController@home');
-//Route::get('/about', 'PagesController@about');
+
 Route::get('/', function(){
-	//$user=Album::orderby("artist")->where('id','!=','1')->take(2)->get();
-	//return $user;
-	/*
-	$user=Album::all();
-	$users=Album::where('id','!=','1')->get();
-	$userid=Album::find(1);
-	$album= new Album;
-	$album-> title="title";
-	$album-> artist= Hash::make("art");
-	$album->save();
-	return Album::all();
-	*/
-
-	/*Album::create([
-		'title'=> 'title',
-		'artist'=> Hash::make("art"),
-
-	]);
-	*/
-/*
-	$album=Album::find(2);
-	$album-> artist='shirin';
-	$album->save();
-*/
-	//$album=Album::find(2);
-	//$album->delete();
-	//return Album::orderby('abs(number)rtist','asc')->take(3)->get();
+	
 	$albums=Album::all();
 	return View::make('album/index',['album'=>$albums]);
 
--- app/views/album/index.blade.php
+++ app/views/album/index.blade.php
@@ -4,15 +4,12 @@
 	</head>
 	<body>
 		<h1>All users</h1>
-		<!--album->toArray());}}-->
-		<?php $var =2; ?>
 		@if (sizeof($album)>0)
 			{{"data availabale"}}
 			@foreach ($album as $album)
 
 					<li>{{ link_to("/album/{$album->artist}",$album->artist)}}</li>
 			@endforeach
-			{{dd($album)}}
 		@else
 			{{"no data availabale"}}
 		@endif
